54b35dc697e92210b92ed14ba34f6fcb9703019e
Denis Timofeev <nakolenke@gmail.com>
2018-02-21 00:33:56 +0300

    Make unique sequence for employee.id

--- src/main/java/ru/code4fun/demo/tmp/dao/entities/Employee.java
+++ src/main/java/ru/code4fun/demo/tmp/dao/entities/Employee.java
@@ -17,7 +17,7 @@ import javax.persistence.Id;
 public class Employee {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String firstName, lastName, description;
