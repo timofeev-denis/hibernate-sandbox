670afee693321859fb0f893a4f03bf4d31a5cbb7
Denis Timofeev <nakolenke@gmail.com>
2018-02-20 23:14:24 +0300

    Create project

---
+++ src/main/java/ru/code4fun/demo/tmp/Tmp1Application.java
@@ -0,0 +1,12 @@
+package ru.code4fun.demo.tmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tmp1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tmp1Application.class, args);
+    }
+}
